13ab6a456accb42874fb6d68b98db37e76342ff6
hye-yeong <ghdwls69@naver.com>
2023-12-22 11:49:02 +0900

    framer-motion 설치

--- src/app/page.js
+++ src/app/page.js
@@ -5,18 +5,15 @@ import Intro from "@/components/Intro";
 import Project from "@/components/Project";
 import Skill from "@/components/Skill";
 import Navbar from "@/components/common/Navbar";
-import styled from "styled-components";
 
 export default function Home() {
   return (
-    <MainContainer>
+    <div>
       <Navbar />
       <Intro id="intro" />
       <About id="about" />
       <Skill id="skills" />
       <Project id="projects" />
-    </MainContainer>
+    </div>
   );
 }
-
-const MainContainer = styled.div``;
