6d5df3cacd9130660b6afd31b5dd2adc96b8cfb7
hye-yeong <ghdwls69@naver.com>
2024-02-05 17:37:41 +0900

    프로필 이미지 추가

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,10 +2,43 @@
 
 import { motion } from "framer-motion";
 import styled from "styled-components";
+import Image from "next/image";
+import profileImg from "../../public/khy.jpeg";
+
+const IntroContainer = styled(motion.section)`
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
+
+  @media (min-width: 1024px) {
+    height: calc(100vh - 4rem);
+  }
+`;
+
+const MotionTextBox = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const MotionText = styled(motion.h1)`
+  font-size: 1.2rem;
+  color: #211c6a;
+`;
+
+const StyledImage = styled(Image)`
+  width: 280px;
+  height: 280px;
+  border-radius: 50%;
+  border: 2px solid #211c6a;
+`;
 
 export default function Intro({ id }) {
   return (
     <IntroContainer id={id}>
+      <StyledImage alt={profileImg} src={profileImg} />
       <MotionTextBox>
         <MotionText
           initial={{ x: -160, y: 20 }}
@@ -41,25 +74,3 @@ export default function Intro({ id }) {
     </IntroContainer>
   );
 }
-
-const IntroContainer = styled(motion.section)`
-  display: flex;
-  justify-content: center;
-
-  @media (min-width: 1024px) {
-    height: calc(100vh - 4rem);
-  }
-`;
-
-const MotionTextBox = styled(motion.div)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-`;
-
-const MotionText = styled(motion.h1)`
-  font-size: 1.2rem;
-  color: #211c6a;
-`;
