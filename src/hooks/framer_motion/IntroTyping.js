6067347a405839b8c224f090538da3e7b36975ff
hye-yeong <ghdwls69@naver.com>
2024-02-06 10:32:26 +0900

    Intro Typing Animation

--- src/hooks/framer_motion/IntroTyping.js
+++ src/hooks/framer_motion/IntroTyping.js
@@ -1,19 +1,35 @@
 import { motion } from "framer-motion";
 import styled from "styled-components";
+import { TypeAnimation } from "react-type-animation";
 
-export default function IntroTyping({ text }) {
+export default function IntroTyping() {
   return (
-    <TypingAnimation
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1 }}
-    >
-      {text}
+    <TypingAnimation>
+      <TypeAnimation
+        sequence={[
+          // Same substring at the start will only be typed out once, initially
+          "김태이는 어떤 사람일까? 책임감 있는 사람",
+          1500, // wait 1s before replacing "Mice" with "Hamsters"
+          "김태이는 어떤 사람일까? 알면 알수록 재밌는 사람",
+          1500,
+          "김태이는 어떤 사람일까? 성실하고 노력하는 사람",
+          1500,
+          "김태이는 어떤 사람일까? 인정할 수 있는 평화주의자",
+          1500,
+          "김태이는 어떤 사람일까? 화가 많이 없지만 화나면 무서운 사람",
+          1500,
+        ]}
+        wrapper="span"
+        speed={20}
+        repeat={Infinity}
+      />
     </TypingAnimation>
   );
 }
 
 const TypingAnimation = styled(motion.h1)`
+  display: inline-block;
+  margin: 3rem 0;
   font-size: 0.8rem;
   color: #2e3a87;
 `;
